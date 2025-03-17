7612fd2517a6a4fa00245dda7ee32946d1531131
zhaoxm <zhaoxm21@mails.tsinghua.edu.cn>
2025-03-17 18:45:24 +0800

    modified the paths to exes

--- exp1/task2.sh
+++ exp1/task2.sh
@@ -2,17 +2,17 @@
 
 set -x
 
-srun -N 2 -n 2 ./mpi_sync 10 100000000 10
-srun -N 2 -n 2 ./mpi_async 10 100000000 10
+srun -N 2 -n 2 mpi_sync 10 100000000 10
+srun -N 2 -n 2 mpi_async 10 100000000 10
 
-srun -N 2 -n 2 ./mpi_sync 10 100000000 20
-srun -N 2 -n 2 ./mpi_async 10 100000000 20
+srun -N 2 -n 2 mpi_sync 10 100000000 20
+srun -N 2 -n 2 mpi_async 10 100000000 20
 
-srun -N 2 -n 2 ./mpi_sync 10 100000000 40
-srun -N 2 -n 2 ./mpi_async 10 100000000 40
+srun -N 2 -n 2 mpi_sync 10 100000000 40
+srun -N 2 -n 2 mpi_async 10 100000000 40
 
-srun -N 2 -n 2 ./mpi_sync 10 100000000 80
-srun -N 2 -n 2 ./mpi_async 10 100000000 80
+srun -N 2 -n 2 mpi_sync 10 100000000 80
+srun -N 2 -n 2 mpi_async 10 100000000 80
 
-srun -N 2 -n 2 ./mpi_sync 10 100000000 160
-srun -N 2 -n 2 ./mpi_async 10 100000000 160
+srun -N 2 -n 2 mpi_sync 10 100000000 160
+srun -N 2 -n 2 mpi_async 10 100000000 160
