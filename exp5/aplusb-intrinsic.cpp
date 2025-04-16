ab4d2961e958ed4942857d9dbc44baf0b9bdaef7
zhaoxm <zhaoxm21@mails.tsinghua.edu.cn>
2025-04-16 11:13:10 +0800

    aplusb_intrinsic v1.0

--- exp5/aplusb-intrinsic.cpp
+++ exp5/aplusb-intrinsic.cpp
@@ -3,4 +3,15 @@
 
 void a_plus_b_intrinsic(float* a, float* b, float* c, int n) {
     // Your code here
+    for (int i = 0; i < n; i += 8) {
+        // 从内存加载 8 个 float 到 AVX 寄存器
+        __m256 vec_a = _mm256_load_ps(a + i);
+        __m256 vec_b = _mm256_load_ps(b + i);
+        
+        // 执行 SIMD 加法
+        __m256 vec_c = _mm256_add_ps(vec_a, vec_b);
+        
+        // 将结果存回内存
+        _mm256_store_ps(c + i, vec_c);
+    }
 }
